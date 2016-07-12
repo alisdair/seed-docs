df92816d9649f3e9da6add49a6b508d0e487973e
Seed <itsjonq@gmail.com>
2016-07-12 14:19:53 -0400

    Adds development script

---
+++ scripts/gulp/tasks/dev.js
@@ -0,0 +1,21 @@
+// Tasks :: Development
+'use strict';
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+// Default task
+gulp.task('development', function(callback) {
+  runSequence(
+    'clean-jekyll',
+    'jekyll',
+    [
+      'styles-base'
+    ],
+    'browsersync',
+    'watch'
+  );
+});
+
+// Alias
+gulp.task('dev', ['development']);
